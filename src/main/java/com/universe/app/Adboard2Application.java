0d6d77f3cc02b7334726812170dd79ca88033e9c
piotr <piotr.muehleisen@gmail.com>
2018-08-11 09:07:55 +0200

    initial commit

---
+++ src/main/java/com/universe/app/Adboard2Application.java
@@ -0,0 +1,12 @@
+package com.universe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adboard2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adboard2Application.class, args);
+	}
+}
---
+++ src/test/java/com/universe/app/Adboard2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.universe.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Adboard2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
